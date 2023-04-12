25be13a383963012c4c75e33a0f08ecf863fd28b
Ilya Boyur <i.boyur@idaproject.com>
2023-04-13 02:07:43 +0300

    Add client tests

---
+++ tests/test_client.py
@@ -0,0 +1,34 @@
+import asyncio
+import pytest
+import uuid
+import json
+from datetime import datetime, timedelta
+
+from db import ChatStorage, Chat, Message, DbEncoder, NotConnectedError, ChatStorageCursor
+from client import Client
+from db import User
+
+
+@pytest.mark.asyncio
+async def test_connect(mocker):
+    patched_send = mocker.patch("client.Client.send", return_value="1 2")
+    client = Client()
+
+    await client.signup()
+
+    patched_send.assert_called_once_with("POST /connect")
+
+
+@pytest.mark.asyncio
+async def test_send(mocker):
+    patched_send = mocker.patch("client.Client.send", return_value="1 2")
+    client = Client()
+    user_id = "fdf40ad1-c49b-4dc1-b8b6-ce2d914a7830"
+    client.force_login(User(user_id))
+
+    await client.post_send(message="hello, world!")
+
+    body = json.dumps(dict(author_id=user_id, chat_id=None, message="hello, world!"))
+    patched_send.assert_called_once_with(f"POST /send {body}")
+
+
