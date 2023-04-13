61da3ec88d41391362bd32ba256fef0915416d9e
Ilya Boyur <i.boyur@idaproject.com>
2023-04-13 11:22:00 +0300

    Move create_storage to conftest

---
+++ tests/conftest.py
@@ -0,0 +1,20 @@
+import asyncio
+import pytest
+import uuid
+from datetime import datetime, timedelta
+
+from db import Message, ChatStorage
+
+
+@pytest.mark.asyncio
+@pytest.fixture
+async def create_storage():
+    db = ChatStorage(max_connections=1)
+    cursor = await asyncio.create_task(db.connect())
+
+    chats = [cursor.create_chat(name="") for _ in range(2)]
+    messages=[Message(msg_id, datetime.now()-timedelta(days=msg_id), uuid.uuid4(), "")
+                      for msg_id in range(2)]
+    [cursor.get_chat(chat).add_message(msg) for chat in chats for msg in messages]
+    cursor.disconnect()
+    return db, chats, messages
--- tests/test_db.py
+++ tests/test_db.py
@@ -1,23 +1,8 @@
 import asyncio
 import pytest
 import uuid
-from datetime import datetime, timedelta
 
-from db import ChatStorage, Chat, Message, DbEncoder, NotConnectedError, ChatStorageCursor
-
-
-@pytest.mark.asyncio
-@pytest.fixture
-async def create_storage():
-    db = ChatStorage(max_connections=1)
-    cursor = await asyncio.create_task(db.connect())
-
-    chats = [cursor.create_chat(name="") for _ in range(2)]
-    messages=[Message(msg_id, datetime.now()-timedelta(days=msg_id), uuid.uuid4(), "")
-                      for msg_id in range(2)]
-    [cursor.get_chat(chat).add_message(msg) for chat in chats for msg in messages]
-    cursor.disconnect()
-    return db, chats, messages
+from db import DbEncoder, NotConnectedError, ChatStorageCursor
 
 
 @pytest.mark.asyncio
