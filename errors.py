4d126007353d298c9d5219853826af919fe04eeb
Ilya Boyur <i.boyur@idaproject.com>
2023-04-13 21:45:11 +0300

    Fix error messages

--- errors.py
+++ errors.py
@@ -1,13 +1,16 @@
 class NotConnectedError(RuntimeError):
     """Operation is rejected because no established connection found"""
-    pass
+    def __init__(self):
+        super().__init__(self.__doc__)
 
 
 class NotExistError(RuntimeError):
     """Requested object is not present in database"""
-    pass
+    def __init__(self):
+        super().__init__(self.__doc__)
 
 
 class MsgLimitExceededError(RuntimeError):
     """Message limit is achieved. Please try again later."""
-    pass
+    def __init__(self):
+        super().__init__(self.__doc__)
