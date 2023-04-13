a714007e2cc8e3b1fcc1d48252720869519c504b
Ilya Boyur <i.boyur@idaproject.com>
2023-04-13 18:22:55 +0300

    Add integration tests

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -33,15 +33,20 @@ def client_other():
 
 
 @pytest.fixture
-def client_auth(client):
-    user_id = "fdf40ad1-c49b-4dc1-b8b6-ce2d914a7830"
-    client.force_login(User(user_id))
-    return client, user_id
+async def create_p2p(client, client_other, server):
+    client.port=server.port
+    await client.signup()
+    client_other.port=server.port
+    await client_other.signup()
+    response = await client.post(
+        "/connect_p2p",
+        data=dict(user_id=client.uuid, other_user_id=client_other.uuid)
+    )
+    return client, client_other, server, json.loads(response)["chat_id"]
 
 
 @pytest.mark.asyncio
 async def test_register(client, server):
-    client.host = server.host
     client.port = server.port
 
     await client.signup()
@@ -50,18 +55,23 @@ async def test_register(client, server):
 
 
 @pytest.mark.asyncio
-@pytest.mark.skip
-async def test_send_message_common(client_auth, server):
-    response = await client_auth.post_send(message="Hello, world!")
+async def test_send_message_common(client, server):
+    client.port = server.port
+    await client.signup()
+
+    TEST_MESSAGE = "test_message"
+    data = dict(
+        author_id=client.uuid,
+        chat_id=None,
+        message=TEST_MESSAGE,
+    )
+
+    response = await client.post("/send", data=data)
     response_json = json.loads(response)
 
     _, chat = server.database.chats.popitem()
     assert len(chat.messages) == 1
-    assert response_json["id"] == chat.messages.pop()
-    print(response_json)
-
-    response = await client.get_status()
-    response = await client.get("/chats", data=dict(user_id=client.uuid))
+    assert response_json["id"] == str(chat.messages.pop().id)
 
 
 @pytest.mark.asyncio
@@ -74,19 +84,40 @@ async def test_connect_p2p(client, client_other, server):
     response = await client.post(
         "/connect_p2p", data=dict(user_id=client.uuid, other_user_id=client_other.uuid)
     )
-    print(response)
     response_json = json.loads(response)
-    print(response_json)
-    assert uuid.UUID(response_json["chat_id"]) in server.database.chats
+    chat_uuid = uuid.UUID(response_json["chat_id"])
+
+    p2p_chat = server.database.chats[chat_uuid]
+    p2p_chat_authors = {str(user.id) for user in p2p_chat.authors}
+    assert client.uuid in p2p_chat_authors
+    assert client_other.uuid in p2p_chat_authors
+    assert p2p_chat.size == 2
+    assert len(p2p_chat.messages) == 0
 
 
 @pytest.mark.asyncio
-@pytest.mark.skip
-async def test_send_message_p2p(client_auth, server, create_p2p):
-    response = await client_auth.post_send(chat_id=chat_id, message="Hello, world!")
+async def test_send_message_p2p(create_p2p):
+    client, client_other, server, chat_id = await create_p2p
+
+    TEST_MESSAGE = "test_message"
+    data = dict(
+        author_id=client.uuid,
+        chat_id=chat_id,
+        message=TEST_MESSAGE,
+    )
+    response = await client.post("/send", data=data)
     response_json = json.loads(response)
 
-    assert len(chat.messages) == 1
+    msg_uuid = uuid.UUID(response_json["id"])
+
+    p2p_chat = server.database.chats[uuid.UUID(chat_id)]
+    p2p_chat_authors = {str(user.id) for user in p2p_chat.authors}
+    assert client.uuid in p2p_chat_authors
+    assert client_other.uuid in p2p_chat_authors
+    assert p2p_chat.size == 2
+    assert len(p2p_chat.messages) == 1
+    assert msg_uuid in {msg.id for msg in p2p_chat.messages}
+
 
 
 @pytest.mark.asyncio
@@ -94,6 +125,18 @@ async def test_send_message_p2p(client_auth, server, create_p2p):
 async def test_get_status(client_auth, server):
     response = await client_auth.post_send(message="Hello, world!")
     response_json = json.loads(response)
+
+
+@pytest.mark.asyncio
+@pytest.mark.skip
+async def test_get_chats(client, server):
+    client.port = server.port
+    await client.signup()
+
+    response = await client_auth.post_send(message="Hello, world!")
+    response_json = json.loads(response)
+
+    response = await client.get("/chats", data=dict(user_id=client.uuid))
 
 # @pytest.mark.asyncio
 # async def test_common_chat(mocker, create_storage):
