6e46e6b9ab8b98dd1a3af75c9ca66e687e588d93
Ilya Boyur <i.boyur@idaproject.com>
2023-04-16 17:51:48 +0300

    Simplify fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -13,7 +13,7 @@ from .factories import ChatFactory, MessageFactory
 @pytest.fixture
 async def create_storage():
     db = ChatStorage(max_connections=1)
-    messages = [MessageFactory() for _ in range(2)]
+    messages = MessageFactory.create_batch(2)
     chats = [ChatFactory(name="", messages=messages) for _ in range(2)]
     db.chats = {chat.id: chat for chat in chats}
     return db, chats, messages
