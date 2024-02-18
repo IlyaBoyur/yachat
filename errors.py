d617c6cadfe1b35230ece05ad6dada7a753b5248
Ilya Boyur <i.boyur@idaproject.com>
2024-02-18 13:15:18 +0300

    Remove code repeats

--- errors.py
+++ errors.py
@@ -1,36 +1,27 @@
-class NotConnectedError(RuntimeError):
+class FromDocStringRuntimeError(RuntimeError):
+
+    def __init__(self):
+        super().__init__(self.__doc__)
+
+
+class NotConnectedError(FromDocStringRuntimeError):
     """Operation is rejected because no established connection found"""
 
-    def __init__(self):
-        super().__init__(self.__doc__)
 
-
-class NotExistError(RuntimeError):
+class NotExistError(FromDocStringRuntimeError):
     """Requested object is not present in database"""
 
-    def __init__(self):
-        super().__init__(self.__doc__)
 
-
-class MsgLimitExceededError(RuntimeError):
+class MsgLimitExceededError(FromDocStringRuntimeError):
     """Message limit is achieved. Please try again later."""
 
-    def __init__(self):
-        super().__init__(self.__doc__)
 
-
-class BannedError(RuntimeError):
+class BannedError(FromDocStringRuntimeError):
     """You have been banned. Please try again later."""
 
-    def __init__(self):
-        super().__init__(self.__doc__)
 
-
-class MaxMembersError(RuntimeError):
+class MaxMembersError(FromDocStringRuntimeError):
     """Max member count is exceeded."""
-
-    def __init__(self):
-        super().__init__(self.__doc__)
 
 
 class ValidationError(RuntimeError):
